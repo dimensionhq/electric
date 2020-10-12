8652cb85fd1fcf255f71a47d8af31603939b915b
pymattia <59345217+pymattia@users.noreply.github.com>
2020-10-12 20:42:32 +0200

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,92 +1,73 @@
-import platform
+from menu_selector import show_menu_selector
 from subprocess import PIPE
-from sys import platform as plat
 from getpass import getuser
+
 import subprocess
-import sys
-from click.decorators import command
 import requests
+import platform
 import zipfile
 import click
+import sys
 import os
-from menu_selector import show_menu_selector
 
 
-def get_architecture() -> str:
+def get_architecture():
     if platform.machine().endswith('64'):
         return 'x64'
-
     if platform.machine().endswith('86'):
         return 'x32'
 
 
-def get_download_url(architecture : str, json : dict) -> str:
-
-    # Decode JSON And Get Correct URL Based On Platform
-    
-    if plat == 'win32':
+def get_download_url(architecture, json):
+    if sys.platform == 'win32':
         if architecture == 'x64':
             return json['win64']
-        if architecture == 'x32':
+        elif architecture == 'x32':
             return json['win32']
-    
-    if plat == 'darwin':
+        
+    elif sys.platform == 'darwin':
         return json['darwin']
     
-    if plat == 'linux':
+    elif sys.platform == 'linux':
         return json['debian']
 
 
-def parse_json_response(json : dict) -> tuple:
+def parse_json_response(json):
     return json['package-name'], json['source'], json['type'], json['switches']
 
 
-def get_setup_name(download_type : str, package_name : str) -> str:
+def get_setup_name(download_type, package_name):
     if plat == 'win32':
-        # Get System Download Path
-        download_path : str = fR'C:\Users\{getuser()}\Downloads\\'
-        # Get System Architecture To Append To SetupFile Name
-        architecture : str = get_architecture()
-
-        # Convert To A List To Manipulate It
-        package : list = package_name.split()
-
-        # Perform Operations To Generate SetupFile Name
+        download_path = 'C:\\Users\\{0}\\Downloads\\'.format(get_user())
+        architecture = get_architecture()
+        package = package_name.split()
         package.insert(0, download_path)
         package.append('Setup')
         package.append(architecture)
         package.append(download_type)
-        
         return ''.join(package)
-
-    if plat == 'darwin':
-        download_path : str = fR'/Users/{getuser()}/Downloads/'
-        package : list = package_name.split()
-
+    
+    elif plat == 'darwin':
+        download_path = rf'/Users/{getuser()}/Downloads/'
+        package = package_name.split()
         package.insert(0, download_path)
         package.append('Setup')
         package.append(download_type)
-
         return ''.join(package)
-
-    if plat == 'linux':
-        download_path : str = fR'/home/{getuser()}/Downloads/'
-        package : list = package_name.split()
-
+    
+    elif plat == 'linux':
+        download_path = rf'/home/{getuser()}/Downloads/'
+        package = package_name.split()
         package.insert(0, download_path)
         package.append('Setup')
         package.append(download_type)
-
         return ''.join(package)
 
 
 # noinspection DuplicatedCode
-def download(url : str, download_type : str, package_name : str):
-
-    # Get Correct File Name 
+def download(url, download_type: str, package_name):
     setup_name = get_setup_name(download_type, package_name)
 
-    # Write Bytes Into The File And Show Progress Bar
     with open(setup_name, "wb") as f:
         response = requests.get(url, stream=True)
         total_length = response.headers.get('content-length')
@@ -95,62 +76,52 @@ def download(url : str, download_type : str, package_name : str):
             f.write(response.content)
         else:
             dl = 0
-            total_length = int(total_length)
+            full_length = int(total_length)
+            
             for data in response.iter_content(chunk_size=4096):
                 dl += len(data)
                 f.write(data)
-                done = int(50 * dl / total_length)
-                sys.stdout.write(f"\r|%s%s| {round(dl / 1000000, 2)} / {round(total_length / 1000000, 2)} MB" % ('█' * done, '░' * (50 - done)))
+                complete = int(50 * dl / total_length)
+                fill_c, unfill_c = chr(9608) * complete, chr(9617) * (50 - complete)
+                sys.stdout.write(f"\r|{fill_c}{unfill_c}| {round(dl / 1000000, 2)} / {round(full_length / 1000000, 2)} MB")
                 sys.stdout.flush()
 
 
-def install_package(package_name : str, switches : list, download_type : str):
+def install_package(package_name, switches, download_type):
+    file_name = get_setup_name(download_type, package_name)
     
-    # Get Path Of File To Run
-    file_name : str = get_setup_name(download_type, package_name)
-
-
     if plat == 'win32':
-
-        # Add Correct Switches If .exe
         if download_type == '.exe':
-            command : str = file_name + ' '
+            command = file_name + ' '
             for switch in switches:
-                command : str = command + ' ' + switch
+                command = command + ' ' + switch
             subprocess.call(command)
-
-        # Add /quiet And Silent Flags If .msi
-        if download_type == '.msi':
-            command : str = 'msiexec.exe /i' + file_name + ' '
-            for switch in switches:
-                command : str = command + ' ' + switch
-            subprocess.call(command)
-
-        # Download And Use Menu Selector For Handling .exe In Zip Files
-        if download_type == '.zip':
             
+        elif download_type == '.msi':
+            command = 'msiexec.exe /i' + file_name + ' '
+            for switch in switches:
+                command = command + ' ' + switch
+            subprocess.call(command)
+            
+        elif download_type == '.zip':
             click.echo(click.style(f'Unzipping File At {file_name}', fg='green'))
-            zip_directory : str = fR'C:\Users\{getuser()}\Downloads\\{package_name}'
-            
+            zip_directory = fR'C:\Users\{getuser()}\Downloads\\{package_name}'
             with zipfile.ZipFile(file_name, 'r') as zip_ref:
                 zip_ref.extractall(zip_directory)
-
-            executable_list : list = []
-
+            executable_list = []
             for name in os.listdir(zip_directory):
                 if name.endswith('.exe'):
                     executable_list.append(name)
-
             executable_list.append('Exit')
-            show_menu_selector(executable_list, fR'C:\Users\{getuser()}\Downloads\\{package_name}')
+            show_menu_selector(executable_list, 'C:\\Users\\{0}\\Downloads\\{1}'.format(get_user(), package_name))
 
     if plat == 'darwin':
-        mount_dmg : str = f'hdiutil attach -nobrowse {file_name}'
+        mount_dmg = f'hdiutil attach -nobrowse {file_name}'
 
 
-def cleanup(download_type : str, package_name : str):
-    setup_name : str = get_setup_name(download_type, package_name)
-    command : str = 'del ' + setup_name
+def cleanup(download_type, package_name):
+    setup_name = get_setup_name(download_type, package_name)
+    command = 'del ' + setup_name
     subprocess.call(command, shell=True)
 
 def run_uninstall(command : str):
