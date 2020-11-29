50efdd21ffd30843e819f90e2791bfcd0e734cb0
TheBossProSniper <tejas.ravishankar0526@gmail.com>
2020-11-29 08:17:39 +0530

    Fixed Auto-Updater

--- src/utils.py
+++ src/utils.py
@@ -638,6 +638,7 @@ def check_supercache_valid():
         date = datetime.strptime(contents, '%Y-%m-%d %H:%M:%S.%f')
         if (datetime.now() - date).days < 1:
             return True
+
         if (datetime.now() - date).days > 3:
             check_for_updates()
     return False
