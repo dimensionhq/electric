647f4cd15e8eeb8685c765009bb315ba341858e8
TheBossProSniper <tejas.ravishankar0526@gmail.com>
2020-12-09 20:07:08 +0530

    Fixed Comment Issue

--- src/electric.py
+++ src/electric.py
@@ -1005,12 +1005,12 @@ if __name__ == '__main__':
     cli()
 
 # Append To $PROFILE For Powershell Completion
-# Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+# Register-ArgumentCompleter -Native -CommandName electric -ScriptBlock {
 #     param($wordToComplete, $commandAst, $cursorPosition)
 #         [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
 #         $Local:word = $wordToComplete.Replace('"', '""')
 #         $Local:ast = $commandAst.ToString().Replace('"', '""')
-#         winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+#         electric complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
 #             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
 #         }
 # }
