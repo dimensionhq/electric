642d0f3c1431855c6a1505e78013dbe183f0e011
TheBossProSniper <tejas.ravishankar0526@gmail.com>
2021-01-07 10:15:48 +0530

    Fixed electricInstall.ps1

--- bin/electricInstall.ps1
+++ bin/electricInstall.ps1
@@ -1,10 +1,10 @@
 Import-Module BitsTransfer
 
 $ErrorActionPreference = "Stop"
-Start-BitsTransfer 'https://srv-store6.gofile.io/download/mwopq6/Electric%20v1.0.0%20Alpha%20Setup.exe' "${Env:\TEMP}\ElectricSetup.exe" -Description 'Downloading Electric Alpha v1.0.0 Setup from https://electric-package-manager.herokuapp.com/install/windows' -DisplayName 'Downloading Electric' -TransferType Download
+Start-BitsTransfer 'https://srv-store1.gofile.io/download/eveHoP/Electric%20v1.0.0%20Alpha%20Setup.exe' "${Env:\TEMP}\ElectricSetup.exe" -Description 'Downloading Electric Alpha v1.0.0 Setup from https://srv-store6.gofile.io/download' -DisplayName 'Downloading Electric' -TransferType Download
 
 Write-Host 'Installing Electric' -ForegroundColor cyan
-& "${Env:\TEMP}\ElectricSetup.exe" /VERYSILENT
+& "${Env:\TEMP}\ElectricSetup.exe" /VERYSILENT | Out-Null
 Write-Host 'Setting Up Tab Completion, Make Sure You Set Your Execution Policy To RemoteSigned Or Unrestricted' -ForegroundColor yellow
 Write-Host 'You Can Do So Using "Set-ExecutionPolicy RemoteSigned" or "Set-ExecutionPolicy Unrestricted"' -ForegroundColor yellow
 if ([System.IO.File]::Exists('C:\Program Files (x86)\Electric\bin\electric.exe')) {
