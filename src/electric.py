34ac472061609165425c5e34c84b3c19e471050b
XtremeDevX <xtremedevx@gmail.com>
2021-02-09 09:41:38 +0530

    Updated electric-settings.json to settings.json

--- src/electric.py
+++ src/electric.py
@@ -48,8 +48,8 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help', '-?'])
 @click.version_option(__version__)
 @click.pass_context
 def cli(_):
-    if not os.path.isfile(rf'{PathManager.get_appdata_directory()}\\electric-settings.json'):
-        click.echo(click.style(f'Creating electric-settings.json at {Fore.CYAN}{PathManager.get_appdata_directory()}{Fore.RESET}', fg='green'))
+    if not os.path.isfile(rf'{PathManager.get_appdata_directory()}\\settings.json'):
+        click.echo(click.style(f'Creating settings.json at {Fore.CYAN}{PathManager.get_appdata_directory()}{Fore.RESET}', fg='green'))
         initialize_settings()
     if not os.path.isdir(PathManager.get_appdata_directory() + r'\Current'):
         os.mkdir(PathManager.get_appdata_directory() + r'\Current')
@@ -1737,8 +1737,8 @@ def show(package_name: str, nightly: bool):
 
 @cli.command(context_settings=CONTEXT_SETTINGS)
 def settings():
-    if not os.path.isfile(rf'{PathManager.get_appdata_directory()}\\electric-settings.json'):
-        click.echo(click.style(f'Creating electric-settings.json at {Fore.CYAN}{PathManager.get_appdata_directory()}{Fore.RESET}', fg='green'))
+    if not os.path.isfile(rf'{PathManager.get_appdata_directory()}\\settings.json'):
+        click.echo(click.style(f'Creating settings.json at {Fore.CYAN}{PathManager.get_appdata_directory()}{Fore.RESET}', fg='green'))
         initialize_settings()
     cursor.hide()
     with Halo('Opening Settings... ', text_color='blue'):
--- src/settings.py
+++ src/settings.py
@@ -3,9 +3,12 @@ from subprocess import Popen, PIPE
 import json
 import os
 
-settings_dir = PathManager.get_appdata_directory() + R'\electric-settings.json'
+settings_dir = PathManager.get_appdata_directory() + R'\settings.json'
 
 def initialize_settings():
+    """
+    Initializes the settings file and directory if it doesn't exist. Writes default settings to the file.
+    """    
     default_electric_settings = {
         "$schema": "https://electric-package-manager.herokuapp.com/schemas/settings",
         "progressBarType": "accented",
@@ -19,9 +22,18 @@ def initialize_settings():
         f.write(json.dumps(default_electric_settings, indent=4))
 
 def read_settings() -> dict:
+    """
+    Reads settings from the settings file in appdata
+
+    Returns:
+        dict: The settings in the form of a dictionary
+    """    
     with open(settings_dir, 'r') as f:
         dictionary = json.load(f)
     return dictionary
 
 def open_settings():
+    """
+    Opens the settings file with the default application configured for .json files.
+    """    
     Popen(f'start {settings_dir}'.split(), stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
