8320d4d65c5b460c0a69148de23bfc177b0ba8f4
XtremeDevX <xtremedevx@gmail.com>
2021-02-24 08:38:57 +0530

    Added More Tests

--- test.py
+++ test.py
@@ -1,29 +1,30 @@
 #   -v, --version
 #   --nightly, --pre-release
-#   -p, --portable, --non-admin  
-#   -vb, --verbose               
-#   -d, --debug                  
-#   -np, --no-progress           
-#   -nc, --no-color              
-#   -l, --log-output        
-#   -dir, --install-dir     
-#   -vc, --virus-check           
-#   -y, --yes                    
-#   -s, --silent                 
-#   -vs, --vscode                
-#   -sb, --sublime               
-#   -ato, --atom                 
-#   -py, --python                
-#   -npm, --node                 
+#   -p, --portable, --non-admin
+#   -vb, --verbose
+#   -d, --debug
+#   -np, --no-progress
+#   -nc, --no-color
+#   -l, --log-output
+#   -dir, --install-dir
+#   -vc, --virus-check
+#   -y, --yes
+#   -s, --silent
+#   -vs, --vscode
+#   -sb, --sublime
+#   -ato, --atom
+#   -py, --python
+#   -npm, --node
 #   -nocache, --no-cache
 #   -sc, --sync
 #   -rd, --reduce
 #   -rl, --rate-limit INTEGER
 #   -f, --force
-#   -cf, --configuration      
-#   -pl, --plugin                
-#   -h, --help               
+#   -cf, --configuration
+#   -pl, --plugin
+#   -h, --help
 
+from subprocess import Popen, PIPE
 from colorama import Fore
 import os
 
@@ -81,4 +82,85 @@ if ec1 == 0 and ec2 == 0:
     print(f'{Fore.GREEN}✅ Nightly / Prerelease Installation And Uninstallation Check ✅{Fore.RESET}')
 else:
     print(f'{Fore.RED}❌ Nightly / Prerelease Installation And Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system('electric install atom --version ')
+ec2 = os.system('electric uninstall atom')
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ Specific Version Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ Specific Version Installation And Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system('electric install sublime-text-3 --no-progress')
+ec2 = os.system('electric uninstall sublime-text-3')
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ No-Progress Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ No-Progress Installation And Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system('electric install sublime-text-3 --no-color')
+ec2 = os.system('electric uninstall sublime-text-3 --no-color')
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ No-Color Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ No-Color Installation And Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system(r'electric install sublime-text-3 --log-output C:\Users\tejas\Desktop\electric-log.txt')
+ec2 = os.system(r'electric uninstall sublime-text-3 --log-output C:\Users\tejas\Desktop\electric-log.txt')
+
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ Logging Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ Logging Installation And Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system(r'electric install sublime-text-3 --install-dir "C:\Users\tejas\Desktop\NewFolder"')
+ec2 = os.system(r'electric uninstall sublime-text-3')
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ Custom Installation Directory Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ Custom Installation Directory Installation And Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system(r'electric install sublime-text --yes')
+ec2 = os.system(r'electric uninstall sublime-text --yes')
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ No-Prompt Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ No-Prompt Installation And Uninstallation Check ❌{Fore.RESET}')
+
+proc = Popen('electric install sublime-text-3 --silent', stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+output, err = proc.communicate()
+if output == b'' and err == b'' and proc.returncode == 0:
+    print(f'{Fore.GREEN}✅ Silent Installation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.GREEN}❌ Silent Installation Check ❌{Fore.RESET}')
+
+proc = Popen('electric uninstall sublime-text-3 --silent', stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+output, err = proc.communicate()
+if output == b'' and err == b'' and proc.returncode == 0:
+    print(f'{Fore.GREEN}✅ Silent Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.GREEN}❌ Silent Uninstallation Check ❌{Fore.RESET}')
+
+ec1 = os.system('electric install aaron-bond.better-comments --vscode')
+ec2 = os.system('electric uninstall aaron-bond.better-comments --vscode')
+
+if ec1 == 0 and ec2 == 0:
+    print(f'{Fore.GREEN}✅ Visual Studio Code Extension Installation And Uninstallation Check ✅{Fore.RESET}')
+else:
+    print(f'{Fore.RED}❌ Visual Studio Code Extension Installation And Uninstallation Check ❌{Fore.RESET}')
+
+# ec1 = os.system('electric install  --sublime')
+# ec2 = os.system('electric uninstall --sublime')
+
+# if ec1 == 0 and ec2 == 0:
+#     print(f'{Fore.GREEN}✅ Sublime Text 3 Extension Installation And Uninstallation Check ✅{Fore.RESET}')
+# else:
+#     print(f'{Fore.RED}❌ Sublime Text 3 Extension Installation And Uninstallation Check ❌{Fore.RESET}')
+
+
 
