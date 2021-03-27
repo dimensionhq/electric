21da7a694b9a6a310e1d68e2d596ce01fd3ae5bf
Sourcery AI <>
2021-03-27 10:12:07 +0000

    'Refactored by Sourcery'

--- src/utils.py
+++ src/utils.py
@@ -1346,10 +1346,7 @@ def send_req_package(package_name: str) -> dict:
                 f'\r| {Fore.LIGHTGREEN_EX}OK{Fore.RESET} |{Fore.LIGHTYELLOW_EX} Initializing Network Debugger{Fore.RESET}')
 
             Debugger.test_internet()
-            sys.exit()
-        else:
-            sys.exit()
-
+        sys.exit()
     try:
         res = json.loads(response.text)
     except JSONDecodeError as e:
@@ -2050,10 +2047,7 @@ def update_package_list():
                     f'\r| {Fore.LIGHTGREEN_EX}OK{Fore.RESET} |{Fore.LIGHTYELLOW_EX} Initializing Network Debugger{Fore.RESET}')
 
                 Debugger.test_internet()
-                sys.exit()
-            else:
-                sys.exit()
-
+            sys.exit()
         data = res.json()
         with open(rf'{PathManager.get_appdata_directory()}\packages.json', 'w+') as f:
             f.write(json.dumps(data, indent=4))
