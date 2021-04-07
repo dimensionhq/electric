716e5b7c67729b548d9ad162788a83b706963053
XtremeDevX <xtremedevx@gmail.com>
2021-04-07 08:52:48 +0530

    Completed Rough Command Completion

--- src/rust/completer/src/main.rs
+++ src/rust/completer/src/main.rs
@@ -1,5 +1,3 @@
-use std::env;
-
 fn main() {
     let commands: Vec<&str> = vec![
         "install",
@@ -18,13 +16,28 @@ fn main() {
     // ["C:\\Users\\xtrem\\Desktop\\Electric\\electric\\src\\rust\\completer\\target\\release\\completer.exe", "--word=in", "--commandline", "electric in", "--position", "11"]
     let args: Vec<String> = std::env::args().collect();
     let typed = &args[3];
-    let parse: Vec<&str> = typed.split(",").collect();
+    let parse: Vec<&str> = typed.split(" ").collect();
 
     if parse.len() == 1 {
         // Command: electric
 
         for command in commands.iter() {
             println!("{}", command);
+        }
+    } else if parse.len() == 2 {
+        // Command: electric install | Command: electric in
+
+        let command = &parse[1];
+
+        if commands.contains(command) {
+            // Complete Package Name Or Flags
+        } else {
+            // Complete Command
+            for cmd in commands.iter() {
+                if cmd.contains(command) {
+                    println!("{}", &cmd);
+                }
+            }
         }
     }
 }
