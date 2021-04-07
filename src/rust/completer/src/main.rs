3914bfeb2f77f4fa12689da7b9fb7fbd19555b46
XtremeDevX <xtremedevx@gmail.com>
2021-04-07 09:27:53 +0530

    Almost Completed Package Completer

--- src/rust/completer/src/main.rs
+++ src/rust/completer/src/main.rs
@@ -1,3 +1,7 @@
+use serde_json::Value;
+use std::fs::read_to_string;
+use std::path::Path;
+
 fn main() {
     let commands: Vec<&str> = vec![
         "install",
@@ -13,9 +17,30 @@ fn main() {
         "list",
     ];
 
+    // Set of flags that can be used with `electric install`
+    let install_flags = vec![
+        "--verbose",
+        "--debug",
+        "--no-progress",
+        "--no-color",
+        "--log-output",
+        "--install-dir",
+        "--virus-check",
+        "--yes",
+        "--silent",
+        "--vscode",
+        "--python",
+        "--node",
+        "--sync",
+        "--reduce",
+        "--rate-limit",
+        "--portable",
+    ];
+
     // ["C:\\Users\\xtrem\\Desktop\\Electric\\electric\\src\\rust\\completer\\target\\release\\completer.exe", "--word=in", "--commandline", "electric in", "--position", "11"]
     let args: Vec<String> = std::env::args().collect();
     let typed = &args[3];
+
     let parse: Vec<&str> = typed.split(" ").collect();
 
     if parse.len() == 1 {
@@ -30,7 +55,10 @@ fn main() {
         let command = &parse[1];
 
         if commands.contains(command) {
-            // Complete Package Name Or Flags
+            // Complete Flags
+            for flag in install_flags.iter() {
+                println!("{}", flag);
+            }
         } else {
             // Complete Command
             for cmd in commands.iter() {
@@ -38,6 +66,48 @@ fn main() {
                     println!("{}", &cmd);
                 }
             }
+        }
+    } else if parse.len() >= 3 {
+        // Command: electric install sublime-text | Command: electric install | Command: electric install --verbose
+        // Autocomplete Flags
+        let command = parse[1].trim();
+
+        match command {
+            "install" => {
+                let word = &args[1].replace("--word=", "");
+
+                if word.starts_with("--") || word == "" {
+                    if word.replace("--", "") == "" {
+                        // Complete All Flags
+                        for flag in install_flags.iter() {
+                            println!("{}", flag);
+                        }
+                    }
+                } else {
+                    // Read Json And Complete Packages
+                    let appdata = std::env::var("APPDATA").unwrap();
+                    let loc = format!(r"{}\electric\packages.json", appdata);
+                    let path = Path::new(loc.as_str());
+
+                    if path.exists() {
+                        let data = read_to_string(loc).unwrap();
+                        let v: Value = serde_json::from_str(data.as_str()).unwrap();
+                        let packages = v["packages"].as_array().unwrap();
+                        let current = args[1].replace("--word=", "");
+
+                        for package in packages.iter() {
+                            let pkg = package.as_str().unwrap().to_string();
+
+                            if pkg.contains(&current) {
+                                println!("{}", pkg);
+                            }
+                        }
+                    } else {
+                        // Setup / Download packages.json
+                    }
+                }
+            }
+            _ => {}
         }
     }
 }
