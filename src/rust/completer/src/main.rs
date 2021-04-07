40a63db390e14225f81a99e11700adae04459f2a
XtremeDevX <xtremedevx@gmail.com>
2021-04-07 22:28:38 +0530

    Added Tab Completion For Major Commands

--- src/rust/completer/src/main.rs
+++ src/rust/completer/src/main.rs
@@ -37,6 +37,20 @@ fn main() {
         "--portable",
     ];
 
+    let uninstall_flags = vec![
+        "--verbose",
+        "--debug",
+        "--no-color",
+        "--log-output",
+        "--yes",
+        "--silent",
+        "--vscode",
+        "--python",
+        "--node",
+        "--no-cache",
+        "--portable"
+    ];
+
     // ["C:\\Users\\xtrem\\Desktop\\Electric\\electric\\src\\rust\\completer\\target\\release\\completer.exe", "--word=in", "--commandline", "electric in", "--position", "11"]
     let args: Vec<String> = std::env::args().collect();
     let typed = &args[3];
@@ -113,7 +127,90 @@ fn main() {
                         // Setup / Download packages.json
                     }
                 }
-            }
+            },  
+            "uninstall" => {
+                let word = &args[1].replace("--word=", "");
+
+                if word.starts_with("--") || word == "" {
+                    if word.replace("--", "") == "" {
+                        // Complete All Flags
+                        for flag in uninstall_flags.iter() {
+                            println!("{}", flag);
+                        }
+                    } else {
+                        // Provide Flag-Specific Completion
+                        for flag in uninstall_flags.iter() {
+                            if flag.contains(word) {
+                                println!("{}", flag);
+                            }
+                        }
+                    }
+                } else {
+                    // Read Json And Complete Packages
+                    let appdata = std::env::var("APPDATA").unwrap();
+                    let loc = format!(r"{}\electric\packages.json", appdata);
+                    let path = Path::new(loc.as_str());
+
+                    if path.exists() {
+                        let data = read_to_string(loc).unwrap();
+                        let v: Value = serde_json::from_str(data.as_str()).unwrap();
+                        let packages = v["packages"].as_array().unwrap();
+                        let current = args[1].replace("--word=", "");
+
+                        for package in packages.iter() {
+                            let pkg = package.as_str().unwrap().to_string();
+
+                            if pkg.contains(&current) {
+                                println!("{}", pkg);
+                            }
+                        }
+                    } else {
+                        // Setup / Download packages.json
+                    }
+                }
+            },
+            "update" => {
+                let word = &args[1].replace("--word=", "");
+
+                if word.starts_with("--") || word == "" {
+                    if word.replace("--", "") == "" {
+                        // Complete All Flags
+                        for flag in install_flags.iter() {
+                            println!("{}", flag);
+                        }
+                    } else {
+                        // Provide Flag-Specific Completion
+                        for flag in install_flags.iter() {
+                            if flag.contains(word) {
+                                println!("{}", flag);
+                            }
+                        }
+                    }
+                } else {
+                    // Read Json And Complete Packages
+                    let appdata = std::env::var("APPDATA").unwrap();
+                    let loc = format!(r"{}\electric\packages.json", appdata);
+                    let path = Path::new(loc.as_str());
+
+                    if path.exists() {
+                        let data = read_to_string(loc).unwrap();
+                        let v: Value = serde_json::from_str(data.as_str()).unwrap();
+                        let packages = v["packages"].as_array().unwrap();
+                        let current = args[1].replace("--word=", "");
+
+                        for package in packages.iter() {
+                            let pkg = package.as_str().unwrap().to_string();
+
+                            if pkg.contains(&current) {
+                                println!("{}", pkg);
+                            }
+                        }
+                    } else {
+                        // Setup / Download packages.json
+                    }
+                }
+            },
+            
             _ => {}
         }
     }
