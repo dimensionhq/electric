4bc0d0fad835b158f1e93050aeaecc2e4634b1ef
XtremeDevX <xtremedevx@gmail.com>
2021-04-07 08:49:03 +0530

    Completed Tab Completion For Basic Commands

--- src/rust/completer/src/main.rs
+++ src/rust/completer/src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let commands: Vec<&str> = vec![
+        "install",
+        "uninstall",
+        "update",
+        "bundle",
+        "search",
+        "new",
+        "config",
+        "sign",
+        "show",
+        "find",
+        "list",
+    ];
+
+    // ["C:\\Users\\xtrem\\Desktop\\Electric\\electric\\src\\rust\\completer\\target\\release\\completer.exe", "--word=in", "--commandline", "electric in", "--position", "11"]
+    let args: Vec<String> = std::env::args().collect();
+    let typed = &args[3];
+    let parse: Vec<&str> = typed.split(",").collect();
+
+    if parse.len() == 1 {
+        // Command: electric
+
+        for command in commands.iter() {
+            println!("{}", command);
+        }
+    }
 }
