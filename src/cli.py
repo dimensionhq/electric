7ef41d5cac2889ebdb873b9fc95217eb82893edd
XtremeDevX <xtremedevx@gmail.com>
2021-12-05 19:19:13 +0400

    fix: bugs

--- src/cli.py
+++ src/cli.py
@@ -16,7 +16,8 @@ from colorama import Fore
 
 __version__ = "0.0.3"
 
-_click7 = click.__version__[0] >= '7'
+_click7 = click.__version__[0] >= "7"
+
 
 class DYMMixin(object):  # pylint: disable=too-few-public-methods
     """
@@ -24,6 +25,7 @@ class DYMMixin(object):  # pylint: disable=too-few-public-methods
     to provide git-like *did-you-mean* functionality when
     a certain command is not registered.
     """
+
     def __init__(self, *args, **kwargs):
         self.max_suggestions = kwargs.pop("max_suggestions", 3)
         self.cutoff = kwargs.pop("cutoff", 0.5)
@@ -43,15 +45,21 @@ class DYMMixin(object):  # pylint: disable=too-few-public-methods
             return super(DYMMixin, self).resolve_command(ctx, args)
         except click.exceptions.UsageError as error:
             error_msg = str(error)
-            matches = difflib.get_close_matches(original_cmd_name,
-                                                self.list_commands(ctx), self.max_suggestions, self.cutoff)
+            matches = difflib.get_close_matches(
+                original_cmd_name,
+                self.list_commands(ctx),
+                self.max_suggestions,
+                self.cutoff,
+            )
             if matches:
-                error_msg += '\n\nDid you mean one of these?\n    %s' % '\n    '.join(matches)  # pylint: disable=line-too-long
+                error_msg += "\n\nDid you mean one of these?\n    %s" % "\n    ".join(
+                    matches
+                )  # pylint: disable=line-too-long
 
             raise click.exceptions.UsageError(error_msg, error.ctx)
 
     def command(self, *args, **kwargs):
-        aliases = kwargs.pop('aliases', [])
+        aliases = kwargs.pop("aliases", [])
         decorator = super(DYMMixin, self).command(*args, **kwargs)
         if not aliases:
             return decorator
@@ -67,7 +75,7 @@ class DYMMixin(object):  # pylint: disable=too-few-public-methods
         return _decorator
 
     def group(self, *args, **kwargs):
-        aliases = kwargs.pop('aliases', [])
+        aliases = kwargs.pop("aliases", [])
         decorator = super(DYMMixin, self).group(*args, **kwargs)
         if not aliases:
             return decorator
@@ -105,24 +113,25 @@ class DYMMixin(object):  # pylint: disable=too-few-public-methods
             cmd = self.get_command(ctx, sub_command)
             if cmd is None:
                 continue
-            if hasattr(cmd, 'hidden') and cmd.hidden:
+            if hasattr(cmd, "hidden") and cmd.hidden:
                 continue
             if sub_command in self._commands:
-                aliases = ','.join(sorted(self._commands[sub_command]))
-                sub_command = '{0} ({1})'.format(sub_command, aliases)
-            cmd_help = cmd.get_short_help_str(limit) if _click7 else cmd.short_help or ''
+                aliases = ",".join(sorted(self._commands[sub_command]))
+                sub_command = "{0} ({1})".format(sub_command, aliases)
+            cmd_help = (
+                cmd.get_short_help_str(limit) if _click7 else cmd.short_help or ""
+            )
             rows.append((sub_command, cmd_help))
 
         if rows:
-            with formatter.section('Commands'):
+            with formatter.section("Commands"):
                 formatter.write_dl(rows)
 
     def format_help(self, ctx, formatter):
         """
         Overrides default click cli help command
-        """        
-        message = f'''Electric Package Manager v1.0.0 Stable Build
-Copyright (c) Electric Inc.
+        """
+        message = f"""Electric Package Manager v1.0.0 Stable Build
 
 Usage: electric <command> [<options>]
 
@@ -144,7 +153,7 @@ Usage: electric <command> [<options>]
   {Fore.LIGHTCYAN_EX}Customization And Cleanup{Fore.RESET}
     {Fore.LIGHTMAGENTA_EX}*{Fore.RESET} feature
     {Fore.LIGHTMAGENTA_EX}*{Fore.RESET} settings
-    {Fore.LIGHTMAGENTA_EX}*{Fore.RESET} cleanup'''
+    {Fore.LIGHTMAGENTA_EX}*{Fore.RESET} cleanup"""
         click.echo(message)
 
 
@@ -154,16 +163,18 @@ class SuperChargeCLI(DYMMixin, click.Group):  # pylint: disable=too-many-public-
     *did-you-mean* functionality when a certain
     command is not found in the group.
     """
+
     # def format_help(self, ctx, formatter):
     #     # Custom Help Message =>
     #     click.echo(click.style('Commands :', fg='bright_green'))
     #     click.echo('Next Line')
 
 
-class DYMCommandCollection(DYMMixin, click.CommandCollection):  # pylint: disable=too-many-public-methods
+class DYMCommandCollection(
+    DYMMixin, click.CommandCollection
+):  # pylint: disable=too-many-public-methods
     """
     click CommandCollection to provide git-like
     *did-you-mean* functionality when a certain
     command is not found in the group.
     """
-
